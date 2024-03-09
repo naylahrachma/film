04399d4d34ffa0c1ae7721761de2a284ee02cf42
naylahrachma <naylahsari556@mhs.mdp.ac.id>
2024-03-09 14:52:57 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Pilem',
+      title: 'Film',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
